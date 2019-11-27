ed09c6cd20cac0743d55824eb0d786994585d1cf
Ian Bailey <iankbailey2@gmail.com>
2019-11-27 14:17:17 -0500

    Initial Commit

---
+++ ParsingJSONLab/ParsingJSONLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParsingJSONLab
+//
+//  Created by Ian Bailey on 11/27/19.
+//  Copyright © 2019 Ian Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
